6d19ef5ec16bb9f85cf8ae744e7fa63164dc78aa
shiyan <1002450926@qq.com>
2018-07-31 14:04:03 +0800

    Revert "BitmapShader绘制圆角"
    This reverts commit 63c80f0

--- app/src/main/java/com/shiyan/app/ui/activity/MainActivity.java
+++ app/src/main/java/com/shiyan/app/ui/activity/MainActivity.java
@@ -2,10 +2,7 @@ package com.shiyan.app.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 
-import com.blankj.utilcode.util.ToastUtils;
 import com.shiyan.app.R;
 
 public class MainActivity extends BaseActivity {
@@ -19,38 +16,7 @@ public class MainActivity extends BaseActivity {
 
         startActivity(new Intent(this,RoundImageViewActivity.class));
 
+
     }
 
-
-    private long currentTime;
-
-    /**
-     * 双击回退(2秒钟)
-     * @param event
-     * @return
-     */
-    @Override
-    public boolean dispatchKeyEvent(KeyEvent event) {
-
-        if(event.getAction() == MotionEvent.ACTION_DOWN) {
-            switch (event.getKeyCode()) {
-
-                case KeyEvent.KEYCODE_BACK:
-
-                    if (System.currentTimeMillis() - currentTime < 2000) {
-
-                        finish();
-                    }
-
-                    currentTime = System.currentTimeMillis();
-
-                    ToastUtils.showShort("再按一次退出");
-
-                    return true;
-            }
-        }
-
-
-        return super.dispatchKeyEvent(event);
-    }
 }
