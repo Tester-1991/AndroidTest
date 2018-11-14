acbdbf9f7050f147587adb7b67c5d6d63a1998e1
shiyan <1002450926@qq.com>
2018-11-14 18:58:01 +0800

    水波纹效果准备集成

---
+++ app/src/main/java/com/shiyan/app/ui/activity/RippleActivity.java
@@ -0,0 +1,17 @@
+package com.shiyan.app.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shiyan.app.R;
+
+public class RippleActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ripple);
+
+
+    }
+}
