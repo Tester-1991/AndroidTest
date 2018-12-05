5c3c92b9b4d0fa649bf0f0e847b1fa969662d3db
android_shiyan <1002450926@qq.com>
2018-12-05 19:21:48 +0800

    测试

--- app/src/main/java/com/shiyan/app/ui/activity/BaseActivity.java
+++ app/src/main/java/com/shiyan/app/ui/activity/BaseActivity.java
@@ -1,26 +1,10 @@
 package com.shiyan.app.ui.activity;
 
-import android.Manifest;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.WindowManager;
 
-import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.ToastUtils;
-import com.shiyan.app.R;
-
-import java.util.List;
-
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.AppSettingsDialog;
-import pub.devrel.easypermissions.EasyPermissions;
 
 public class BaseActivity extends AppCompatActivity{
 
@@ -33,6 +17,17 @@ public class BaseActivity extends AppCompatActivity{
         //全屏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                     WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    public void test(){
+        ThreadLocal<String> threadLocal = new ThreadLocal(){
+            @Override
+            protected Object initialValue() {
+                return "1";
+            }
+        };
+
+        ToastUtils.showShort(threadLocal.get());
     }
 
 
--- app/src/main/java/com/shiyan/app/ui/activity/CustomBehavior2Activity.java
+++ app/src/main/java/com/shiyan/app/ui/activity/CustomBehavior2Activity.java
@@ -1,6 +1,5 @@
 package com.shiyan.app.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.shiyan.app.R;
@@ -11,5 +10,6 @@ public class CustomBehavior2Activity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_behavior2);
+
     }
 }
